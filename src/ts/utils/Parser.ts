a84ec9985eb9ab29ae89e936e90286227ce123af
marcinRG <marcin.guszpit@gmail.com>
2018-01-17 23:06:20 +0100

    changes

--- src/ts/utils/Parser.ts
+++ src/ts/utils/Parser.ts
@@ -7,9 +7,10 @@ import {IMatchResult} from '../model/interfaces/IMatchResult';
 import {ITag} from '../model/interfaces/ITag';
 import {ICreateNode} from '../model/interfaces/ICreateNode';
 import {
-    findInArray, findParserRuleWithHTMLTag, findParserRuleWithTag, findRegexStrWithTag,
+    findParserRuleWithHTMLTag, findParserRuleWithTag,
     getAllAllowedChildrenRegexStr
 } from './TagsRulesUtils';
+import {getMatchResults} from './MatchResultsUtils';
 
 export class Parser {
 
@@ -42,8 +43,7 @@ export class Parser {
     }
 
     public getParserRuleByTag(tag: string): IParserRule {
-        const rule = findParserRuleWithTag(tag, this.rules);
-        return rule ? rule : null;
+        return findParserRuleWithTag(tag, this.rules);
     }
 
     public getParserRuleByHTMLTag(htmlTag: string): IParserRule {
@@ -51,7 +51,7 @@ export class Parser {
     }
 }
 
-const parseText = (text: string, parentNode: INode, rules: IParserRule[], tags: ITag[], tagsFactory: ICreateNode) => {
+function parseText(text: string, parentNode: INode, rules: IParserRule[], tags: ITag[], tagsFactory: ICreateNode) {
     if (text && text.length) {
         const rule = findParserRuleWithHTMLTag(parentNode.getNodeName(), tags, rules);
         if (rule) {
@@ -63,9 +63,9 @@ const parseText = (text: string, parentNode: INode, rules: IParserRule[], tags: 
         }
         parseText(text, parentNode, rules, tags, tagsFactory);
     }
-};
+}
 
-const getText = (text: string, regExp: RegExp): string => {
+function getText(text: string, regExp: RegExp): string {
     const results = text.search(regExp);
     if (results > 0) {
         return text.substring(0, results);
@@ -73,15 +73,15 @@ const getText = (text: string, regExp: RegExp): string => {
     if (results < 0) {
         return text;
     }
-};
+}
 
-const findAndAddTextNode = (text: string, regexp: RegExp, parentNode: INode): string => {
+function findAndAddTextNode(text: string, regexp: RegExp, parentNode: INode): string {
     const txtNew = getText(text, regexp);
     return createTextNode(text, txtNew, parentNode);
-};
+}
 
-const findAndAddTagNode = (text: string, regexp: RegExp, tagName: string, parentNode: INode,
-                           rules: IParserRule[], tags: ITag[], tagsFactory: ICreateNode): string => {
+function findAndAddTagNode(text: string, regexp: RegExp, tagName: string, parentNode: INode,
+                           rules: IParserRule[], tags: ITag[], tagsFactory: ICreateNode): string {
     const results = text.match(regexp);
     if (results) {
         const matchResult: IMatchResult = getMatchResults(results, tagName, rules);
@@ -100,84 +100,19 @@ const findAndAddTagNode = (text: string, regexp: RegExp, tagName: string, parent
         }
     }
     return text;
-};
+}
 
-const createTextNode = (text: string, textToAdd: string, parentNode: INode): string => {
+function createTextNode(text: string, textToAdd: string, parentNode: INode): string {
     if (textToAdd) {
         const len = (textToAdd && textToAdd.length) ? textToAdd.length : text.length;
         parentNode.addNode(new TextNode(textToAdd));
         text = text.substring(len, text.length);
     }
     return text;
-};
+}
 
-const createNode = (matchResult: IMatchResult, tagsFactory: ICreateNode): INode => {
+function createNode(matchResult: IMatchResult, tagsFactory: ICreateNode): INode {
     if (matchResult) {
         return tagsFactory.createNode(matchResult);
     }
-};
-
-const getMatchResults = (results: RegExpMatchArray, tagName: string, rules: IParserRule[]) => {
-    if (results) {
-        const pos = findFirstInMatchResult(results);
-        const rule = findChildTagRule(pos, tagName, rules);
-        if (rule) {
-            const matchResult: IMatchResult = rule.values(results, pos);
-            if (matchResult) {
-                return matchResult;
-            }
-        }
-        return null;
-    }
-};
-
-const findFirstInMatchResult = (tab: string[]) => {
-    if ((tab) && (tab.length > 1)) {
-        for (let i = 1; i < tab.length; i++) {
-            if (tab[i]) {
-                return i;
-            }
-        }
-    }
-    return -1;
-};
-
-const getExistingChildrenNodes = (position: number, rules: IParserRule[]) => {
-    if (position <= rules.length) {
-        const tab = [];
-        const childrenRules = rules[position].allowedChildrenNodes;
-        for (const elem of childrenRules) {
-            const regexStr = findRegexStrWithTag(elem, rules);
-            if (regexStr) {
-                tab.push(elem);
-            }
-        }
-        return tab;
-    }
-};
-
-const findChildTagRule = (index: number, tag: string, rules: IParserRule[]): IParserRule => {
-    const pos = findInArray(tag, 'name', rules);
-    if (pos >= 0 && index >= 0) {
-        let diff = index - 1;
-        const childrenNodes = getExistingChildrenNodes(pos, rules);
-        if (childrenNodes && childrenNodes.length > 0) {
-            for (const val of childrenNodes) {
-                if (diff === 0) {
-                    return rules[findInArray(val, 'name', rules)];
-                }
-                if (diff > 0) {
-                    diff = diff - getMatchGroups(val, rules);
-                }
-            }
-        }
-    }
-    return null;
-};
-
-const getMatchGroups = (tag: string, rules: IParserRule[]): number => {
-    const pos = findInArray(tag, 'name', rules);
-    if (pos >= 0) {
-        return rules[pos].matchGroups;
-    }
-};
+}
